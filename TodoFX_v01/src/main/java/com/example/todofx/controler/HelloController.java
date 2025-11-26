f1a0a40cf348d1d3b53d02ac37fc9aec5e7be2a7
Aras Adaib <arasadaib@web.de>
2025-11-26 13:01:49 +0100

    Todo.java FXML hinzugefügt

---
+++ TodoFX_v01/src/main/java/com/example/todofx/controler/HelloController.java
@@ -0,0 +1,45 @@
+package com.example.todofx.controler;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+
+
+    //------------SAVE------------
+    @FXML
+    private TextField taskField;
+
+    @FXML
+    private DatePicker deadlineField;
+
+    //-------------TableView-------------
+
+    @FXML
+    private TableView<?> tableView;
+
+
+
+    @FXML
+    private TableColumn<?, ?> deadlineCol;
+
+    @FXML
+    private TableColumn<?, ?> stateCol;
+
+    @FXML
+    private TableColumn<?, ?> taskCol;
+
+
+
+
+    @FXML
+    void onSave(ActionEvent event) {
+
+    }
+
+}
